627c141844f6fd77ee2bce022c687bc7c2a20b97
BasitSameerpro <summerkhan054@gmail.com>
2025-01-31 23:16:07 +0500

    Cart System

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
